413016fd664b081fe48e8f041ed8ece4775678d7
Abubakar Yasir <abubakaryasir569@gmail.com>
2022-03-01 18:10:41 +0500

    setting API constants

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const port = 3000;
 const app = express();
 
 app.get("/", function (req, res) {
-    http.get(
-        "http://api.weatherapi.com/v1/current.json?key=71ae3d627a7a45b3902201828222802&q=Lahore&aqi=yes"
-    );
+    const url =
+        "http://api.weatherapi.com/v1/current.json?key=71ae3d627a7a45b3902201828222802&q=Lahore&aqi=yes";
+
+    http.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const cityName = weatherData.location.name;
+            const countryName = weatherData.location.country;
+            const temp = weatherData.current.temp_c;
+            const condition = weatherData.current.condition.text;
+        });
+    });
     res.send("Server is Up and Running");
     console.log("Server is Up and Running");
 });
