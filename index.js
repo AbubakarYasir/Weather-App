066984cb9eb8d1983c26eed5ef8d1ab032e49f20
Abubakar Yasir <abubakaryasir569@gmail.com>
2022-03-02 12:09:48 +0500

    Setting up Response

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const http = require("http");
 const port = 3000;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
+    const chosenCity = "Lahore";
     const url =
-        "http://api.weatherapi.com/v1/current.json?key=71ae3d627a7a45b3902201828222802&q=Lahore&aqi=yes";
+        "http://api.weatherapi.com/v1/current.json?key=71ae3d627a7a45b3902201828222802&q=" +
+        chosenCity +
+        "&aqi=yes";
 
     http.get(url, function (response) {
         console.log(response.statusCode);
@@ -17,9 +23,9 @@ app.get("/", function (req, res) {
             const countryName = weatherData.location.country;
             const temp = weatherData.current.temp_c;
             const condition = weatherData.current.condition.text;
+            res.send("The temperature in " + chosenCity + " is " + temp + " degree Celcius.");
         });
     });
-    res.send("Server is Up and Running");
     console.log("Server is Up and Running");
 });
 
